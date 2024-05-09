2d8c6d5cf32ee15bfabf071209e769934074e922
Lindsey Weche <lindseyhaishanw22@icloud.com>
2024-05-09 00:35:36 -0400

    footer1

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,17 @@ const Submenu = document.querySelector(".sub-menu");
 Menu.addEventListener("click",(e) =>{
     Submenu.classList.toggle("hide");
 });
+
+const mainContent = (c) => document.createElement(c);
+const Footer = document.getElementById("bottom");
+
+
+const para = Footer.appendChild(mainContent("bottom"));
+para.innerHTML = 
+`<p> &copy;Author: Lindsey Weche</p>
+<p>
+  <ahref="mailto:lindseyhaishanw22@icloud.com">lindseyhaishanw22@icloud.com</a>
+</p>`;
+
+
+
